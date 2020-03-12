dd568d05d55fd2fd29369ecc8d7eaddba6fa2bc3
David <garciamartindavid@hotmail.es>
2020-03-12 18:14:43 +0100

    Ya escoge palabra al azar y arreglado bug de espacios

--- app/src/main/java/com/example/ahorcado/VentanaAhorcado.java
+++ app/src/main/java/com/example/ahorcado/VentanaAhorcado.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,7 +16,7 @@ import android.view.ViewGroup;
 public class VentanaAhorcado extends Fragment {
 
     public VentanaAhorcado() {
-        // Required empty public constructor
+
     }
 
 
